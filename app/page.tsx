dec3730dfa0d141b434e6e5c6053c00e02545caf
Jordan Dennis <jdenn105@gmail.com>
2024-07-26 14:10:04 +1000

    Toggling the theme.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,38 @@
+'use client';
+
 import Image from "next/image";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+function ThemeToggleButton() {
+  var themeName = "dark";
+
+  function toggleTheme() {
+    if (themeName === "dark") {
+      themeName = "light";
+      document.getElementById("themeToggleButton").innerHTML = "<i class=\"bi bi-sun\"/>";
+      document.documentElement.setAttribute("data-bs-theme", "light");
+    } else {
+      themeName = "dark";
+      document.getElementById("themeToggleButton").innerHTML = "<i class=\"bi bi-moon\"/>";
+      document.documentElement.setAttribute("data-bs-theme", "dark");
+    }
+  }
+
+  return (
+    <div class="row justify-content-end">
+      <div class="col-auto m-3">
+        <button type="button" class="btn" id="themeToggleButton" onClick={toggleTheme}>
+          <i className="bi bi-moon"/>
+        </button>
+      </div>
+    </div>
+  );
+}
 
 export default function Home() {
   return (
     <main>
+      <ThemeToggleButton/>
       <div className="row justify-content-center m-3">
         <div className="col-auto">
           <h1>
