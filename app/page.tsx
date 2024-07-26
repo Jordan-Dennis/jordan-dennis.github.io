7f804a9705c9d9fd2ff04ae9b909580b65932edb
Jordan Dennis <jdenn105@gmail.com>
2024-07-26 12:49:16 +1000

    New webpage (see #28).

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="row justify-content-center m-3">
+        <div className="col-auto">
+          <h1>
+            Jordan Dennis        
+          </h1>
+        </div>
+      </div>
+      <div className="container">
+
+      </div>
+    </main>
+  );
+}
