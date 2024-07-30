b840f6a96a572fbe1fa7254d1cf5cc130d2ae539
Jordan Dennis <jdenn105@gmail.com>
2024-07-30 11:05:18 +1000

    Experimenting with a nav-bar.

--- app/page.tsx
+++ app/page.tsx
@@ -4,26 +4,83 @@ import Image from "next/image";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 function ThemeToggleButton() {
-  var themeName = "dark";
+  var darkTheme = true;
 
   function toggleTheme() {
-    if (themeName === "dark") {
-      themeName = "light";
-      document.getElementById("themeToggleButton").innerHTML = "<i class=\"bi bi-sun\"/>";
+    if (darkTheme) {
+      document.getElementById("themeToggleIcon").setAttribute("class", "bi bi-sun");
       document.documentElement.setAttribute("data-bs-theme", "light");
     } else {
-      themeName = "dark";
-      document.getElementById("themeToggleButton").innerHTML = "<i class=\"bi bi-moon\"/>";
+      document.getElementById("themeToggleIcon").setAttribute("class", "bi bi-moon");
       document.documentElement.setAttribute("data-bs-theme", "dark");
     }
+    darkTheme = !darkTheme;
   }
 
   return (
-    <div class="row justify-content-end">
-      <div class="col-auto m-3">
-        <button type="button" class="btn" id="themeToggleButton" onClick={toggleTheme}>
-          <i className="bi bi-moon"/>
-        </button>
+    <button type="button" className="btn" onClick={toggleTheme}>
+      <i className="bi bi-moon" id="themeToggleIcon"/>
+    </button>
+  );
+}
+
+function LinkedInProfile() {
+  return (
+    <a className="btn" href="https://www.linkedin.com/in/jordan-dennis-3238bb281/">
+      <i className="bi bi-linkedin"/>
+    </a>
+  );
+}
+
+function GithubProfile() {
+  return (
+    <a className="btn" href="https://github.com/jordan-dennis">
+      <i className="bi bi-github"/>
+    </a>
+  );
+}
+
+function Socials() {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <div className="nav-link active">
+          <i className="bi bi-house"/>
+        </div>
+      </li>
+      <li className="nav-item">
+        <GithubProfile/>
+      </li>
+      <li className="nav-item">
+        <LinkedInProfile/>
+      </li>
+    </ul>
+  );
+}
+
+
+function PageTitle({ children }) {
+  return (
+    <div className="row justify-content-center m-3">
+      <div className="col-auto">
+        <h1>
+          { children }       
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+function About({ children }) {
+  return (
+    <div className="m-3">
+      <h2 className="ms-3">
+        About
+      </h2>
+      <div className="p-3 border rounded">
+        <p className="p-0 m-0">
+          { children }
+        </p>
       </div>
     </div>
   );
@@ -32,17 +89,14 @@ function ThemeToggleButton() {
 export default function Home() {
   return (
     <main>
+      <Socials/>
       <ThemeToggleButton/>
-      <div className="row justify-content-center m-3">
-        <div className="col-auto">
-          <h1>
-            Jordan Dennis        
-          </h1>
-        </div>
-      </div>
-      <div className="container">
-
-      </div>
+      <PageTitle>
+        Jordan Dennis
+      </PageTitle>
+      <About>
+        Hi, thanks for visiting my site. It's currently under construction.
+      </About>
     </main>
   );
 }
